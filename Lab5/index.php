85ca8d92d1426735f3bdfe92a2fd494de1a4251e
tryton-vanmeer <tyke505@hotmail.com>
2015-10-18 17:09:45 -0400

    Updated Lab 5

--- Lab5/index.php
+++ Lab5/index.php
@@ -13,21 +13,21 @@
   <body>
     <?php
     echo "Tryton Van Meer";
-    echo "<br>"
+    echo "<br>";
     echo "040790151";
-    echo "<br>"
+    echo "<br>";
     echo "Hello, World! I am using PHP!";
-    echo "<br>"
+    echo "<br>";
     echo "The course is going fine";
-    echo "<br>"
+    echo "<br>";
     echo '<a href="../Lab1/index.html">Lab 1</a>';
-    echo "<br>"
+    echo "<br>";
     echo '<a href="../Lab1/index.html">Lab 2</a>';
-    echo "<br>"
+    echo "<br>";
     echo '<a href="../Lab1/index.html">Lab 3</a>';
-    echo "<br>"
+    echo "<br>";
     echo '<a href="../Lab1/index.html">Lab 4</a>';
-    echo "<br>"
+    echo "<br>";
     echo '<a href="../Assignment1/index.html">Assignment 1</a>';
     ?>
   </body>
