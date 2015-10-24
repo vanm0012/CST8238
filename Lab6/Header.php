00d13ca521b5745e6c20ee4af292e0943b91d298
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 17:19:03 -0400

    Menu resides in Header

--- Lab6/Header.php
+++ Lab6/Header.php
@@ -1,9 +1,11 @@
 <?php
+include("Menu.php");
+
 function _header()
 {
-  echo '<nav class="purple darken-2 white-text center">
-    <h1>Tryton Van Meer | 040790151 | LAb 6 | Logical and Conditional Statements</h1>
-    </nav>
-  ';
+  echo '<nav class="purple darken-2 white-text center">';
+  _menu();
+  echo '<h1>Tryton Van Meer | 040790151 | LAb 6 | Logical and Conditional Statements</h1>
+    </nav>';
 }
 ?>
