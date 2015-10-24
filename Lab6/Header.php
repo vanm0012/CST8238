2c181aeca8c3780930f8f1f9ec3b9c2cc1397be2
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 17:44:43 -0400

    Changed LAb to Lab in H1

--- Lab6/Header.php
+++ Lab6/Header.php
@@ -3,8 +3,8 @@ include("Menu.php");
 
 function _header()
 {
-  echo '<nav class="purple darken-2 white-text center">';
-  echo '<h1>Tryton Van Meer | 040790151 | LAb 6 | Logical and Conditional Statements</h1>
-    </nav>';
+  echo '<nav class="purple darken-2 white-text center">
+          <h1>Tryton Van Meer | 040790151 | Lab 6 | Logical and Conditional Statements</h1>
+        </nav>';
 }
 ?>
