6899ac53accd3703b4cf75b8cfeb3209402a3ff4
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 14:39:45 -0400

    Added _footer function

--- Lab6/Footer.php
+++ Lab6/Footer.php
@@ -1,3 +1,10 @@
 <?php
-
+function _footer()
+{
+  echo '<div class="footer blue white-text valign-wrapper center">
+    <span>Student Number: 040790151 • Name: Tryton Van Meer • Email: vanm0012@algonquinlive.com</span>
+    <a class="right" href="https://github.com/vanm0012/CST8238">
+      <span class="octicon octicon-mark-github black-text"></span>
+  ';
+}
 ?>
