c97e0c915a0bb21dd170d6b544a0e5e8d0ccf761
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 15:28:48 -0400

    Fixed li tags in _menu()

--- Lab6/Menu.php
+++ Lab6/Menu.php
@@ -3,13 +3,13 @@ function _menu()
 {
   echo '<div class="menu">
     <ul class="white-text">
-    <li class="blue"
+    <li class="blue">
       <a href="All.php">All</a>
     </li>
-    <li class="blue"
+    <li class="blue">
       <a href="Odd.php">Odd</a>
     </li>
-    <li class="blue"
+    <li class="blue">
       <a href="Ten.php">Ten</a>
     </li>
   ';
