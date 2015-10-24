5dfaba6799648ffb558b198b7966b530fa70e803
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 14:30:03 -0400

    Added Title

--- Lab6/All.php
+++ Lab6/All.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
+<?php
+include("Header.php");
+include("Menu");
+include("Footer.php");
+?>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="author" content="tryton-vanmeer">
-    <title></title>
+    <title>Lab 6</title>
     <link href="css/Stylesheet.css" rel="stylesheet" type="text/css">
   </head>
   <body>
     <?php
+    _header();
 
     ?>
   </body>
