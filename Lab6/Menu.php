9d6716233b792aa57dce48af8ed3ce35c4343471
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 18:51:30 -0400

    Removed li color class

--- Lab6/Menu.php
+++ Lab6/Menu.php
@@ -4,13 +4,13 @@ function _menu()
   echo '
     <aside class="sidebar blue">
       <ul class="white-text">
-        <li class="puple accent-2">
+        <li>
           <a href="All.php">All</a>
         </li>
-        <li class="puple accent-2">
+        <li>
           <a href="Odd.php">Odd</a>
         </li>
-        <li class="puple accent-2">
+        <li>
           <a href="Ten.php">Ten</a>
         </li>
       </ul>
