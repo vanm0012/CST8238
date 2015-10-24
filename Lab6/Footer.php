3b72f648b0ef6c421a0796efa644d6e0b1d871cb
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 14:42:35 -0400

    Edited footer for lab 6

--- Lab6/Footer.php
+++ Lab6/Footer.php
@@ -2,7 +2,7 @@
 function _footer()
 {
   echo '<div class="footer blue white-text valign-wrapper center">
-    <span>Student Number: 040790151 • Name: Tryton Van Meer • Email: vanm0012@algonquinlive.com</span>
+    <span>Lab 6 • Logical and Conditional Statements</span>
     <a class="right" href="https://github.com/vanm0012/CST8238">
       <span class="octicon octicon-mark-github black-text"></span>
   ';
