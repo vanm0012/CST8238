1eca0b2892d9fde9bf2d9f4f3884696648d41234
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 17:24:22 -0400

    Removed menu

--- Lab6/Header.php
+++ Lab6/Header.php
@@ -4,7 +4,6 @@ include("Menu.php");
 function _header()
 {
   echo '<nav class="purple darken-2 white-text center">';
-  _menu();
   echo '<h1>Tryton Van Meer | 040790151 | LAb 6 | Logical and Conditional Statements</h1>
     </nav>';
 }
