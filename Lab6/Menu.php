f88e0b414f799d4af889fdb8ff6dec9bc60082c3
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 16:30:43 -0400

    Fixed HTML structure

--- Lab6/Menu.php
+++ Lab6/Menu.php
@@ -2,17 +2,19 @@
 function _menu()
 {
   echo '
-    <ul class="sidebar">
-      <li class="blue">
-        <a href="All.php">All</a>
+    <div id="sidebar">
+      <ul class="white-text">
+        <li class="blue">
+          <a href="All.php">All</a>
         </li>
         <li class="blue">
-        <a href="Odd.php">Odd</a>
+          <a href="Odd.php">Odd</a>
         </li>
         <li class="blue">
-        <a href="Ten.php">Ten</a>
+          <a href="Ten.php">Ten</a>
         </li>
-        </ul>
+      </ul>
+    </div>
   ';
 }
 ?>
