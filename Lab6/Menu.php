0c2f32d55a47ac537dfd176f27656b735d915bda
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 15:51:21 -0400

    Fixed closing tags

--- Lab6/Menu.php
+++ Lab6/Menu.php
@@ -2,16 +2,18 @@
 function _menu()
 {
   echo '<div class="sidebar">
-    <ul class="white-text">
-    <li class="blue">
-      <a href="All.php">All</a>
-    </li>
-    <li class="blue">
-      <a href="Odd.php">Odd</a>
-    </li>
-    <li class="blue">
-      <a href="Ten.php">Ten</a>
-    </li>
+      <ul class="white-text">
+      <li class="blue">
+        <a href="All.php">All</a>
+        </li>
+        <li class="blue">
+        <a href="Odd.php">Odd</a>
+        </li>
+        <li class="blue">
+        <a href="Ten.php">Ten</a>
+        </li>
+      </ul>
+    </div>
   ';
 }
 ?>
