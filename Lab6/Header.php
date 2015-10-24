d8339a27ceee5a83f661e62b4993461ef200c306
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 14:19:11 -0400

    Fixed Lab 6 to Lab6

---
+++ Lab6/Header.php
@@ -0,0 +1,9 @@
+<?php
+function _header()
+{
+  echo '<nav class="purple darken-2 white-text center">
+    <h1>Tryton Van Meer | 040790151 | LAb 6 | Logical and Conditional Statements</h1>
+    </nav>
+  ';
+}
+?>
