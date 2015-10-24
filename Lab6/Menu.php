d88fa793af6ebae966c0000206ed2e0af312a7b7
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 15:07:35 -0400

    Created _menu function

--- Lab6/Menu.php
+++ Lab6/Menu.php
@@ -1,3 +1,17 @@
 <?php
-
+function _menu()
+{
+  echo '<div class="menu">
+    <ul class="white-text">
+    <li class="blue"
+      <a href="All.php">All</a>
+    </li>
+    <li class="blue"
+      <a href="Odd.php">Odd</a>
+    </li>
+    <li class="blue"
+      <a href="Ten.php">Ten</a>
+    </li>
+  ';
+}
 ?>
