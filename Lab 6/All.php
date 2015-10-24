30a21096d36bcf8049662e78ee509de46d1f8670
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 14:05:38 -0400

    Added Lab 6

---
+++ Lab 6/All.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="tryton-vanmeer">
+    <title></title>
+    <link href="css/Stylesheet.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <?php
+
+    ?>
+  </body>
+</html>
---
+++ Lab 6/Ten.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <title></title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <?php
+
+    ?>
+  </body>
+</html>
