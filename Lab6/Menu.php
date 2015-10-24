3cc5c6560a96772e5766950a1594d40a8e810d38
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 15:52:06 -0400

    Fixed indentations

--- Lab6/Menu.php
+++ Lab6/Menu.php
@@ -3,14 +3,14 @@ function _menu()
 {
   echo '<div class="sidebar">
       <ul class="white-text">
-      <li class="blue">
-        <a href="All.php">All</a>
+        <li class="blue">
+          <a href="All.php">All</a>
         </li>
         <li class="blue">
-        <a href="Odd.php">Odd</a>
+          <a href="Odd.php">Odd</a>
         </li>
         <li class="blue">
-        <a href="Ten.php">Ten</a>
+          <a href="Ten.php">Ten</a>
         </li>
       </ul>
     </div>
