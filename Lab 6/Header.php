145a81df6700499574872e38f54252903b4bcbf9
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 14:19:53 -0400

    Removed Lab 6 folder

--- Lab 6/Header.php
+++
@@ -1,9 +0,0 @@
-<?php
-function _header()
-{
-  echo '<nav class="purple darken-2 white-text center">
-    <h1>Tryton Van Meer | 040790151 | LAb 6 | Logical and Conditional Statements</h1>
-    </nav>
-  ';
-}
-?>
