11f47c73986cb345c4b55867f63e57859fd1a606
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 16:06:32 -0400

    Fixed up HTML structure

--- Lab6/Menu.php
+++ Lab6/Menu.php
@@ -1,19 +1,18 @@
 <?php
 function _menu()
 {
-  echo '<div class="sidebar">
-      <ul class="white-text">
-        <li class="blue">
-          <a href="All.php">All</a>
+  echo '
+    <ul class="sidebar">
+      <li class="blue">
+        <a href="All.php">All</a>
         </li>
         <li class="blue">
-          <a href="Odd.php">Odd</a>
+        <a href="Odd.php">Odd</a>
         </li>
         <li class="blue">
-          <a href="Ten.php">Ten</a>
+        <a href="Ten.php">Ten</a>
         </li>
-      </ul>
-    </div>
+        </ul>
   ';
 }
 ?>
