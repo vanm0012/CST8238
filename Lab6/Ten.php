bad00a7d89db29c767e24b7e1c65bfed68cb8916
tryton-vanmeer <tyke505@hotmail.com>
2015-10-24 14:24:43 -0400

    Added Includes and using _header()

--- Lab6/Ten.php
+++ Lab6/Ten.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
+<?php
+include("Header.php");
+include("Menu");
+include("Footer.php");
+?>
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
+    <meta name="author" content="tryton-vanmeer">
     <title></title>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="css/Stylesheet.css" rel="stylesheet" type="text/css">
   </head>
   <body>
     <?php
+    _header();
 
     ?>
   </body>
