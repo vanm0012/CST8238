4b172ced7b51a27c2080a954325d6975ce0217ed
vanm0012 <vanm0012@algonquinlive.com>
2015-11-01 12:45:27 -0500

    Fixed array access in for loop

--- Lab7/Array1.php
+++ Lab7/Array1.php
@@ -25,7 +25,7 @@ include("Footer.php");
       <?php
       for ($x=0; $x<count($calendar); $x++)
       {
-        echo "Month $x: $calendar($x)";
+        echo "Month $x: $calendar[$x]";
         echo "<br>";
       }
 
