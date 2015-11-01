a5698f2b15080b18beb5f19ba70411e131a472ec
vanm0012 <vanm0012@algonquinlive.com>
2015-11-01 11:05:47 -0500

    Updated Footer for lab7

--- Lab7/Footer.php
+++ Lab7/Footer.php
@@ -2,7 +2,7 @@
 function _footer()
 {
   echo '<div class="footer amber white-text valign-wrapper center">
-    <span>Lab 6 • Logical and Conditional Statements</span>
+    <span>Lab 6 • Programming Arrays, Objects</span>
     <a class="right" href="https://github.com/vanm0012/CST8238">
       <span class="octicon octicon-mark-github black-text"></span>
   ';
