4ecfcd49a22106d8b097733f399607c5d6358450
vanm0012 <vanm0012@algonquinlive.com>
2015-11-01 14:07:07 -0500

    Answered the questions

--- Lab7/Array2.php
+++ Lab7/Array2.php
@@ -19,25 +19,29 @@ include("Footer.php");
       <div class="center">
       <ol>
         <li>What is the difference between a numeric and associative array?
-          <br>A:
+          <p>A numeric array uses indexs, which are numeric of course, and values are accessed by #array[index].
+          Associative arrays use keys and function like a dictionary. Values are accessed by $array['key']</p>
         </li>
         <li>What is the main benefit of array keyword?
-
+          <p></p>
         </li>
         <li>What if the difference between foreach and each?
-
+          <p>In PHP, foreach is a method to loop through an entire array.
+          each only returns the current key/value pair, then advances the array cursor.
+        So, for the 'each' command, you would have to use it for every value you wish to access.</p>
         </li>
-        <li>Ho can you create a mulidimensional array?
-
+        <li>How can you create a mulidimensional array?
+          <p>To create a multidimensional array in PHP, you would create an array of arrays.
+          For example: $array = array(array(1,2,3), array("a","b","c"), array("i","ii","iii")).</p>
         </li>
         <li>How can you determine the number of elements there are in an array?
-
+          <p>count($array) will return a count of items in an array in PHP.</p>
         </li>
-        <li>What is the purpose of the explde function?
-
+        <li>What is the purpose of the explode function?
+          <p>To split a string into substrings a defined by a delimiter.</p>
         </li>
         <li>How can you set PHP's internal pointer into an array back to the first element of the array?
-
+          <p>'reset(&amp;$array)' will return internal pointer back to the start, in PHP.</p>
         </li>
       </ol>
     </div>
