3ff2b14de5dc3b4bb2054d07f037938d4c1a4068
vanm0012 <vanm0012@algonquinlive.com>
2015-11-01 11:05:12 -0500

    Updated Header for lab 7

--- Lab7/Header.php
+++ Lab7/Header.php
@@ -2,7 +2,7 @@
 function _header()
 {
   echo '<nav class="amber darken-2 white-text center">
-          <h1>Tryton Van Meer | 040790151 | Lab 6 | Logical and Conditional Statements</h1>
+          <h1>Tryton Van Meer | 040790151 | Lab 6 | Programming Arrays, Objects</h1>
         </nav>';
 }
 ?>
