7df57f7120a7a4203a10bd5787877e7661995538
vanm0012 <vanm0012@algonquinlive.com>
2015-11-01 10:50:08 -0500

    Added Lab 7 files

---
+++ Lab7/Menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <title></title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <?php
+
+    ?>
+  </body>
+</html>
