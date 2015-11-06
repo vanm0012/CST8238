c406f32c2fb3697552247a776e5e16529de89b0f
vanm0012 <vanm0012@algonquinlive.com>
2015-11-06 06:16:12 -0800

    Removed center class from content

--- Assignment2/Question1.php
+++ Assignment2/Question1.php
@@ -17,7 +17,7 @@ include("Footer.php");
     <?php _header(); ?>
     <main>
       <?php _menu(); ?>
-      <div class="center content">
+      <div class="content">
     <?php
 
     /* For loop : left aligned */
