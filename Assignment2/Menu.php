3a27a077959ed81be221718f6309bb25e423b1a4
vanm0012 <vanm0012@algonquinlive.com>
2015-11-06 06:17:32 -0800

    Added rest of Question pages to menu

--- Assignment2/Menu.php
+++ Assignment2/Menu.php
@@ -7,6 +7,15 @@ function _menu()
         <li>
           <a href="Question1.php">Question 1</a>
         </li>
+        <li>
+          <a href="Question2.php">Question 2</a>
+        </li>
+        <li>
+          <a href="Question3.php">Question 3</a>
+        </li>
+        <li>
+          <a href="Question4.php">Question 4</a>
+        </li>
       </ul>
     </aside>
   ';
