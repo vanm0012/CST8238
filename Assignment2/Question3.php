084ffc4f0f75b89bcdde1eeba3a3dc5bf01f67f0
vanm0012 <vanm0012@algonquinlive.com>
2015-11-07 11:15:21 -0500

    Added function for first part

--- Assignment2/Question3.php
+++ Assignment2/Question3.php
@@ -4,6 +4,19 @@ include("Header.php");
 include("Menu.php");
 include("Footer.php");
 ?>
+<?php
+function calc_average($arr)
+{
+  $arr_sum = 0;
+  foreach($arr as $val)
+  {
+    $arr_sum = $arr_sum + $val;
+  }
+  $arr_avg = $arr_sum / count($arr);
+
+  echo "The average is: $arr_avg <br>";
+}
+?>
 <html>
   <head>
     <meta charset="utf-8">
@@ -19,7 +32,10 @@ include("Footer.php");
       <?php _menu(); ?>
       <div class="center-box">
     <?php
+    $q3_array1 = array(9, 3, 1, 0, 99, 2, 5, 6, 32, 1 ,55);
+    $q3_array2 = array(9, 18, 1, 0, 23, 22, 4, 6, 5, 32, 55);
 
+    calc_average($q3_array1);
     ?>
       </div>
     </main>
