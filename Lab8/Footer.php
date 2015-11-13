b7d638e27f1856ed545d57a60a39fd3ff812075f
vanm0012 <vanm0012@algonquinlive.com>
2015-11-13 10:29:24 -0500

    Updated colour scheme

--- Lab8/Footer.php
+++ Lab8/Footer.php
@@ -1,7 +1,7 @@
 <?php
 function _footer()
 {
-  echo '<div class="footer lime white-text valign-wrapper center">
+  echo '<div class="footer green white-text valign-wrapper center">
     <span>Lab 8 • Forms and Session State</span>
     <a class="right" href="https://github.com/vanm0012/CST8238">
       <span class="octicon octicon-mark-github black-text"></span>
--- Lab8/Header.php
+++ Lab8/Header.php
@@ -1,7 +1,7 @@
 <?php
 function _header()
 {
-  echo '<nav class="lime darken-2 white-text center">
+  echo '<nav class="green darken-2 white-text center">
           <h1>Tryton Van Meer | 040790151 | Lab 8 | Forms and Session State</h1>
         </nav>';
 }
