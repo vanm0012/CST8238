6dc1a44b76ab9b51e18b626d76ed1944050d9f55
vanm0012 <vanm0012@algonquinlive.com>
2015-11-13 08:47:36 -0500

    Edited for lab 8

--- Lab8/Footer.php
+++ Lab8/Footer.php
@@ -2,7 +2,7 @@
 function _footer()
 {
   echo '<div class="footer lime white-text valign-wrapper center">
-    <span>Lab 6 • Programming Arrays, Objects</span>
+    <span>Lab 8 • Forms and Session State</span>
     <a class="right" href="https://github.com/vanm0012/CST8238">
       <span class="octicon octicon-mark-github black-text"></span>
   ';
--- Lab8/Header.php
+++ Lab8/Header.php
@@ -2,7 +2,7 @@
 function _header()
 {
   echo '<nav class="lime darken-2 white-text center">
-          <h1>Tryton Van Meer | 040790151 | Lab 6 | Programming Arrays, Objects</h1>
+          <h1>Tryton Van Meer | 040790151 | Lab 8 | Forms and Session State</h1>
         </nav>';
 }
 ?>
