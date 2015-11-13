fb0ea07bc9cb60cfcdb8bfcd86611273b42c44ce
vanm0012 <vanm0012@algonquinlive.com>
2015-11-13 11:47:13 -0500

    Added roboto font to style

--- Lab8/Input.php
+++ Lab8/Input.php
@@ -30,6 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     <title>Lab 8</title>
     <link href="css/Stylesheet.css" rel="stylesheet" type="text/css">
     <link href="css/octicons.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
   </head>
   <body>
     <?php _header(); ?>
--- Lab8/Session1.php
+++ Lab8/Session1.php
@@ -12,6 +12,7 @@ include("Footer.php");
     <title>Lab 8</title>
     <link href="css/Stylesheet.css" rel="stylesheet" type="text/css">
     <link href="css/octicons.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
   </head>
   <body>
     <?php _header(); ?>
