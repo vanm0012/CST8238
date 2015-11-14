5a9e62e58f3a2e53148b685389c9e365b6193b21
vanm0012 <vanm0012@algonquinlive.com>
2015-11-14 11:42:51 -0500

    Removed js folder

--- Lab8/js/scripts.js
+++
@@ -1,7 +0,0 @@
-// Add hypens to text input
-function addHyphen() {
-    var t = document.forms['the_form'].elements['the_text'];
-      if (t.value.length > 0) {
-        t.value = t.value.substring(0,4) + "-" + t.value.substring(4, t.value.length);
-      }
-}
