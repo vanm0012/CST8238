a30ddfc09f848bcec29b0e9bdd7b6fb44072d1aa
vanm0012 <vanm0012@algonquinlive.com>
2015-11-22 18:43:18 -0500

    Added Footer, Menu, and Header PHP. Also added stylesheet

---
+++ Lab9/Header.php
@@ -0,0 +1,8 @@
+<?php
+function _header()
+{
+  echo '<nav class="blue darken-2 white-text center">
+          <h1>Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</h1>
+        </nav>';
+}
+?>
---
+++ Lab9/Menu.php
@@ -0,0 +1,20 @@
+<?php
+function _menu()
+{
+  echo '
+    <div class="menu">
+      <ul class="white-text">
+        <li class="yellow">
+          <a href="CreateAccount.php">Input</a>
+        </li>
+        <li class="yellow">
+          <a href="Login.php">Session 1</a>
+        </li>
+        <li class="yellow">
+            <a href="ViewAllAccounts.php">Session 2</a>
+        </li>
+      </ul>
+    </div>
+  ';
+}
+?>
