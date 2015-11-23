35e6f1058e4580034c8bf3e57ae621a4ca9de589
vanm0012 <vanm0012@algonquinlive.com>
2015-11-22 19:17:55 -0500

    Redone header with header tag

--- Lab9/Header.php
+++ Lab9/Header.php
@@ -3,8 +3,8 @@ include("Menu.php");
 
 function _header()
 {
-  echo '<nav class="blue darken-2 white-text center">
-          <h1 style="text-align:left;">Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</h1>';
+  echo '<header style="text-align:left;">Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</header>';
+  echo '<nav class="blue darken-2 white-text center">';
           _menu();
   echo  '</nav>';
 }
