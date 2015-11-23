5328aa73cc05de2c079f1739f1dad8592689d377
vanm0012 <vanm0012@algonquinlive.com>
2015-11-22 19:02:01 -0500

    Fixed Nav header and menu

--- Lab9/Header.php
+++ Lab9/Header.php
@@ -4,8 +4,8 @@ include("Menu.php");
 function _header()
 {
   echo '<nav class="blue darken-2 white-text center">
-          <h1>Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</h1>
+          <h1 style="text-align:left;">Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</h1>';
           _menu();
-        </nav>';
+  echo  '/nav>';
 }
 ?>
