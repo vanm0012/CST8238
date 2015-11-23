07622f8c144793804927b8d10596cfd07b18e5b9
vanm0012 <vanm0012@algonquinlive.com>
2015-11-22 19:19:58 -0500

    Fixed up header

--- Lab9/Header.php
+++ Lab9/Header.php
@@ -3,8 +3,8 @@ include("Menu.php");
 
 function _header()
 {
-  echo '<header style="text-align:left;">Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</header>';
-  echo '<nav class="blue darken-2 white-text center">';
+  echo '<header class="blue darken-2 white-text">Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</header>';
+  echo '<nav class="blue darken-2 white-text">';
           _menu();
   echo  '</nav>';
 }
