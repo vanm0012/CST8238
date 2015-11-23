6021eb788a7540066ec5b2df799c892251c1b5ce
vanm0012 <vanm0012@algonquinlive.com>
2015-11-22 20:29:22 -0500

    Changed h1 to h2

--- Lab9/Header.php
+++ Lab9/Header.php
@@ -4,7 +4,7 @@ include("Menu.php");
 function _header()
 {
   echo '<div class="header blue darken-2 white-text">';
-  echo '<h1>Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</h1>';
+  echo '<h2>Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</h2>';
   echo '<nav>';
           _menu();
   echo  '</nav>';
