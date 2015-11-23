fc687b252d107d6f2462391c9b8375d81bfa6f94
vanm0012 <vanm0012@algonquinlive.com>
2015-11-22 19:03:01 -0500

    Fixed nav tag

--- Lab9/Header.php
+++ Lab9/Header.php
@@ -6,6 +6,6 @@ function _header()
   echo '<nav class="blue darken-2 white-text center">
           <h1 style="text-align:left;">Tryton Van Meer | 040790151 | Lab 9 | PHP and MySQL</h1>';
           _menu();
-  echo  '/nav>';
+  echo  '</nav>';
 }
 ?>
