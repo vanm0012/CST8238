37ac7e2fbbd5f430bafdd1fcbc8add4979767f35
vanm0012 <vanm0012@algonquinlive.com>
2015-11-22 19:08:24 -0500

    Removed div for Menu

--- Lab9/Menu.php
+++ Lab9/Menu.php
@@ -2,19 +2,17 @@
 function _menu()
 {
   echo '
-    <div class="menu">
-      <ul class="white-text">
-        <li class="yellow">
-          <a href="CreateAccount.php">Input</a>
-        </li>
-        <li class="yellow">
-          <a href="Login.php">Session 1</a>
-        </li>
-        <li class="yellow">
-            <a href="ViewAllAccounts.php">Session 2</a>
-        </li>
-      </ul>
-    </div>
+    <ul class="menu white-text">
+      <li class="yellow">
+        <a href="CreateAccount.php">Input</a>
+      </li>
+      <li class="yellow">
+        <a href="Login.php">Session 1</a>
+      </li>
+      <li class="yellow">
+          <a href="ViewAllAccounts.php">Session 2</a>
+      </li>
+    </ul>
   ';
 }
 ?>
